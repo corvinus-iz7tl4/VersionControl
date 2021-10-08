a95dd295ee83dfaaebd218d2348016d359138883
vagob <vagob@DESKTOP-P1MNCE9>
2021-10-08 17:57:36 +0200

    adatok betöltése (4.feladat)

--- NegyedikHet_IZT7L4/NegyedikHet_IZT7L4/Form1.cs
+++ NegyedikHet_IZT7L4/NegyedikHet_IZT7L4/Form1.cs
@@ -12,9 +12,16 @@ namespace NegyedikHet_IZT7L4
 {
     public partial class Form1 : Form
     {
+        List<Flat> Flats;
+        RealEstateEntities context = new RealEstateEntities();
         public Form1()
         {
             InitializeComponent();
+            LoadData();
+        }
+        void LoadData()
+        {
+            Flats = context.Flats.ToList();
         }
     }
 }
