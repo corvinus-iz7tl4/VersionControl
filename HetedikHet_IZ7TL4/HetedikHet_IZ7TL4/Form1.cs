7fd0489b0f59ae9df750fe6b3e25c52ac4bc0998
vagob <vagob@DESKTOP-P1MNCE9>
2021-10-29 21:16:41 +0200

    6/5.

--- HetedikHet_IZ7TL4/HetedikHet_IZ7TL4/Form1.cs
+++ HetedikHet_IZ7TL4/HetedikHet_IZ7TL4/Form1.cs
@@ -9,6 +9,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using System.Xml;
 
 namespace HetedikHet_IZ7TL4
 {
@@ -18,12 +19,12 @@ namespace HetedikHet_IZ7TL4
         public Form1()
         {
             InitializeComponent();
-            AdatokLekérdezés();
+            GetExchange();
 
             dataGridView1.DataSource = Rates;
         }
 
-        void AdatokLekérdezés()
+        void GetExchange()
         {
             var mnbService = new MNBArfolyamServiceSoapClient();
 
@@ -37,6 +38,27 @@ namespace HetedikHet_IZ7TL4
             var response = mnbService.GetExchangeRates(request);
 
             var result = response.GetExchangeRatesResult;
+
+            var xml = new XmlDocument();
+            xml.LoadXml(result);
+
+            foreach (XmlElement element in xml.DocumentElement)
+            {
+                var rate = new RateData();
+                Rates.Add(rate);
+
+                rate.Date = DateTime.Parse(element.GetAttribute("date"));
+
+                var childElement = (XmlElement)element.ChildNodes[0];
+                rate.Currency = childElement.GetAttribute("curr");
+
+                var unit = decimal.Parse(childElement.GetAttribute("unit"));
+                var value = decimal.Parse(childElement.InnerText);
+                if (unit != 0)
+                    rate.Value = value / unit;
+            }
+
+
         }
     }
 }
