f34d18e71370e37502d1e802f11e5d824052dc6e
vagob <vagob@DESKTOP-P1MNCE9>
2021-10-29 20:53:59 +0200

    6/3.

--- HetedikHet_IZ7TL4/HetedikHet_IZ7TL4/Form1.cs
+++ HetedikHet_IZ7TL4/HetedikHet_IZ7TL4/Form1.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using HetedikHet_IZ7TL4.MnbServiceReference;
+using System;
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
@@ -15,6 +16,23 @@ namespace HetedikHet_IZ7TL4
         public Form1()
         {
             InitializeComponent();
+            AdatokLekérdezés();
+        }
+
+        void AdatokLekérdezés()
+        {
+            var mnbService = new MNBArfolyamServiceSoapClient();
+
+            var request = new GetExchangeRatesRequestBody()
+            {
+                currencyNames = "EUR",
+                startDate = "2020-01-01",
+                endDate = "2020-06-30"
+            };
+
+            var response = mnbService.GetExchangeRates(request);
+
+            var result = response.GetExchangeRatesResult;
         }
     }
 }
