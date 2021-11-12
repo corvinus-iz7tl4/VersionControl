08185ce5bfdac71d050b11459417017a45b4071f
vagob <vagob@DESKTOP-P1MNCE9>
2021-11-12 19:11:32 +0100

    Absztrakció felhasználása

--- NyolcadikHet_IZ7TL4/NyolcadikHet_IZ7TL4/Entities/CarFactory.cs
+++ NyolcadikHet_IZ7TL4/NyolcadikHet_IZ7TL4/Entities/CarFactory.cs
@@ -7,11 +7,11 @@ using System.Threading.Tasks;
 
 namespace NyolcadikHet_IZ7TL4.Entities
 {
-    public class BallFactory:IToyFactory
+    class CarFactory:IToyFactory
     {
         public Toy CreateNew()
         {
-            return new Ball();
+            return new Car();
         }
     }
 }
