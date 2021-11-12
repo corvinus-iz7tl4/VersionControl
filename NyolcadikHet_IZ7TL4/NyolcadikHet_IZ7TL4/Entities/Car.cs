376c23e26fdb2838465b8d9d1d12a3dfa9c63f70
vagob <vagob@DESKTOP-P1MNCE9>
2021-11-12 19:21:39 +0100

    Absztrakció felhasználása futószalagon

--- NyolcadikHet_IZ7TL4/NyolcadikHet_IZ7TL4/Entities/Car.cs
+++ NyolcadikHet_IZ7TL4/NyolcadikHet_IZ7TL4/Entities/Car.cs
@@ -10,7 +10,7 @@ namespace NyolcadikHet_IZ7TL4.Entities
 {
     class Car : Toy
     {
-        protected override void DrawImage(Graphics graphics)
+        protected override void DrawImage(Graphics g)
         {
             Image imageFile = Image.FromFile("Images/car.png");
             g.DrawImage(imageFile, new Rectangle(0, 0, Width, Height));
--- NyolcadikHet_IZ7TL4/NyolcadikHet_IZ7TL4/Form1.cs
+++ NyolcadikHet_IZ7TL4/NyolcadikHet_IZ7TL4/Form1.cs
@@ -1,4 +1,5 @@
-﻿using NyolcadikHet_IZ7TL4.Entities;
+﻿using NyolcadikHet_IZ7TL4.Abstraction;
+using NyolcadikHet_IZ7TL4.Entities;
 using System;
 using System.Collections.Generic;
 using System.ComponentModel;
@@ -13,9 +14,9 @@ namespace NyolcadikHet_IZ7TL4
 {
     public partial class Form1 : Form
     {
-        private List<Ball> _balls = new List<Ball>();
-        private BallFactory _factory;
-        public BallFactory Factory 
+        private List<Toy> _toys = new List<Toy>();
+        private IToyFactory _factory;
+        public IToyFactory Factory 
         {
             get { return _factory; }
             set { _factory = value; }
@@ -24,13 +25,13 @@ namespace NyolcadikHet_IZ7TL4
         public Form1()
         {
             InitializeComponent();
-            Factory = new BallFactory();
+            Factory = new CarFactory();
         }
 
         private void createTimer_Tick(object sender, EventArgs e)
         {
             var ball = Factory.CreateNew();
-            _balls.Add(ball);
+            _toys.Add(ball);
             ball.Left = -ball.Width;
             mainPanel.Controls.Add(ball);
         }
@@ -38,18 +39,18 @@ namespace NyolcadikHet_IZ7TL4
         private void conveyorTimer_Tick(object sender, EventArgs e)
         {
             var maxPosition = 0;
-            foreach (var ball in _balls)
+            foreach (var ball in _toys)
             {
-                ball.MoveBall();
+                ball.MoveToy();
                 if (ball.Left > maxPosition)
                     maxPosition = ball.Left;
             }
 
             if (maxPosition > 1000)
             {
-                var oldestBall = _balls[0];
+                var oldestBall = _toys[0];
                 mainPanel.Controls.Remove(oldestBall);
-                _balls.Remove(oldestBall);
+                _toys.Remove(oldestBall);
             }
         }
     }
