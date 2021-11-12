3c95ec90cdd5100af21e258de84935f96429a1fd
vagob <vagob@DESKTOP-P1MNCE9>
2021-11-12 18:19:18 +0100

    Labda

---
+++ NyolcadikHet_IZ7TL4/NyolcadikHet_IZ7TL4/Ball.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace NyolcadikHet_IZ7TL4
+{
+    class Ball:Label
+    {
+        public Ball()
+        {
+            AutoSize = false;
+            Height = 50;
+            Width = 50;
+            Paint += Ball_Paint;
+        }
+
+        private void Ball_Paint(object sender, PaintEventArgs e)
+        {
+            DrawImage(e.Graphics); 
+        }
+
+        protected void DrawImage(Graphics graphics)
+        {
+            graphics.FillEllipse(new SolidBrush(Color.Blue), 0, 0, Width, Height);
+        }
+        public void MoveBall()
+        {
+            Left += 1;
+        }
+    }
+}
