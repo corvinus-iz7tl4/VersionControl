c21acfddcc6998725916c977d097dfdced000d4d
vagob <vagob@DESKTOP-P1MNCE9>
2021-12-09 21:03:01 +0100

    12/2

--- TizenkettedikHet_IZ7TL4/TizenkettedikHet_IZ7TL4/Form1.cs
+++ TizenkettedikHet_IZ7TL4/TizenkettedikHet_IZ7TL4/Form1.cs
@@ -20,6 +20,9 @@ namespace TizenkettedikHet_IZ7TL4
             InitializeComponent();
             ga = gc.ActivateDisplay();
             this.Controls.Add(ga);
+
+            /*gc.AddPlayer();
+            gc.Start(true);*/
         }
     }
 }
